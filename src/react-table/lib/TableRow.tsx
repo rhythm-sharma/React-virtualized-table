6d8a9de43188e30ab8828c76ea311036e1282856
rrhythmsharma <rrhythmsharma@gmail.com>
2022-04-17 21:54:30 +0530

    Fix: word break in table cell

--- src/react-table/lib/TableRow.tsx
+++ src/react-table/lib/TableRow.tsx
@@ -10,7 +10,7 @@ type Props = TableRowProps;
 
 const TableRow: React.FC<Props> = (props) => {
   return (
-    <tr {...props} className="h-24 border-gray-300 border-b">
+    <tr {...props} className="h-24 border-gray-300 border-b break-all	">
       {props.children}
     </tr>
   );
