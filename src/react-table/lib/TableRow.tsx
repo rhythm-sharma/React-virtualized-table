0af9025448665e6c44c526425a853dbf0e16d2c2
rrhythmsharma <rrhythmsharma@gmail.com>
2022-04-17 18:33:16 +0530

    Added: Table Row Cell with onClick support

---
+++ src/react-table/lib/TableRow.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface TableRowProps {
+  children: any;
+  style: object;
+  onClick: (e: React.SyntheticEvent) => void;
+}
+
+type Props = TableRowProps;
+
+const TableRow: React.FC<Props> = (props) => {
+  return (
+    <tr {...props} className="h-24 border-gray-300 border-b">
+      {props.children}
+    </tr>
+  );
+};
+
+export default React.memo(TableRow);
